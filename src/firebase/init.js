8928aa7b64e2841e54f4f3df8efc49c06a68973f
Patrick Peter Zohdy <zohdy@me.com>
2018-05-13 23:11:24 +0200

    added firebase listener

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,7 +2,7 @@
 import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
-const config = {
+let config = {
   apiKey: "AIzaSyCWp0z-gZ8RRPAZK3uoLLNEmZ_Sq4Q82Co",
   authDomain: "vue-zohdy-chat.firebaseapp.com",
   databaseURL: "https://vue-zohdy-chat.firebaseio.com",
@@ -10,7 +10,8 @@ const config = {
   storageBucket: "vue-zohdy-chat.appspot.com",
   messagingSenderId: "397601689575"
 };
+
 const firebaseApp = firebase.initializeApp(config);
-firebaseApp.firestore().settings( { timestampInSnapshots: true } );
+firebaseApp.firestore().settings({timestampsInSnapshots: true});
 
 export default firebaseApp.firestore();
