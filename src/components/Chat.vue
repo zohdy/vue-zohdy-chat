ec192106c23d520e5aae002ab0ad973e98e59000
Patrick Peter Zohdy <zohdy@me.com>
2018-05-13 23:44:00 +0200

    added "moment" for date formatting and auto-bottom-scrolling

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,7 +3,7 @@
     <h2 class="center teal-text">Zohdy Chat</h2>
     <div class="card">
       <div class="card-content">
-        <ul class="messages">
+        <ul class="messages" v-chat-scroll>
           <li v-for="message in messages" :key="message.id">
             <span class="teal-text">{{ message.name }}</span>
             <span class="grey-text text-darken-3">{{ message.content }}</span>
@@ -21,6 +21,7 @@
 <script>
   import NewMessage from '@/components/NewMessage'
   import db from '@/firebase/init'
+  import moment from 'moment'
 
   export default {
     name: 'Chat',
@@ -35,6 +36,7 @@
     },
     created(){
       let ref = db.collection('messages').orderBy('timestamp');
+
       ref.onSnapshot(snapshot => {
         snapshot.docChanges.forEach(change => {
           if(change.type === 'added'){
@@ -43,7 +45,7 @@
               id: doc.id,
               name: doc.data().name,
               content: doc.data().content,
-              timestamp: doc.data().timestamp
+              timestamp: moment(doc.data().timestamp).format('lll')
             });
           }
         });
@@ -65,7 +67,24 @@
 
   .chat .time {
     display:block;
-    font-size: 1.2em;
+    font-size: 0.8em;
+  }
+
+  .messages{
+    max-height: 300px;
+    overflow: auto;
+  }
+
+  .messages::-webkit-scrollbar {
+    width: 3px;
+  }
+
+  .messages::-webkit-scrollbar-track {
+    background: #ddd;
+  }
+
+  .messages::-webkit-scrollbar-thumb {
+    background: #aaa;
   }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueChatScroll from 'vue-chat-scroll'
 
-Vue.config.productionTip = false
+Vue.use(VueChatScroll);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
